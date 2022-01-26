400cf6eb5126624c2760c0d1d5fb3b3a1e4b3e50
nelsonh88 <nelson.huezo88@gmail.com>
2022-01-26 16:25:36 -0500

    Updates logic for quiz and styling     - Created a helper fn to hold the questions     - Brought in files that hold the questions and answers for me to refer to     - Updated the quiz logic to display questions and answer options     - Updated logic to check ig answer is correct

--- src/App.js
+++ src/App.js
@@ -1,7 +1,60 @@
+import React, { useState } from "react";
+import { myQuestions as questions } from "./helpers/helpers";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const [score, setScore] = useState(0);
+
+  const clickAnswerHandler = (isCorrect) => {
+    if (isCorrect) {
+      setScore((previousValue) => {
+        return previousValue + 1;
+      });
+      alert("You are correct");
+    }
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true);
+    }
+  };
+
+  return (
+    <div className="app">
+      {/* HINT: replace "false" with logic to display the 
+      score when the user has answered all the questions */}
+      {showScore ? (
+        <div className="score-section">
+          You scored {score} out of {questions.length}
+        </div>
+      ) : (
+        <>
+          <div className="question-section">
+            <div className="question-count">
+              <span>Question {currentQuestion + 1}</span>/{questions.length}
+            </div>
+            <div className="question-text">
+              {questions[currentQuestion].question}
+            </div>
+          </div>
+          <div className="answer-section">
+            {questions[currentQuestion].answerOptions.map((answerOption) => {
+              return (
+                <button
+                  onClick={() => clickAnswerHandler(answerOption.isCorrect)}
+                >
+                  {answerOption.answerText}
+                </button>
+              );
+            })}
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/helpers/helpers.js
@@ -0,0 +1,50 @@
+export const myQuestions = [
+  {
+    question:
+      "You need to mark individuals as attended as they show up to your customer appreciation dinner. Which channel and program type should you use?",
+    answerOptions: [
+      {
+        answerText: "Webinar channel and Event program type",
+        isCorrect: false,
+      },
+      {
+        answerText: "Email Send channel and Email program type",
+        isCorrect: false,
+      },
+      {
+        answerText: "Live Event channel and Event program type",
+        isCorrect: true,
+      },
+      {
+        answerText: "Operational program channel and Default program type",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    question:
+      "Which of the following is not a way to become a member of a program?",
+
+    answerOptions: [
+      {
+        answerText:
+          "Create a new lead via the iPad or Android event application",
+        isCorrect: false,
+      },
+      {
+        answerText: "Fill out a form that's located in Design Studio",
+        isCorrect: true,
+      },
+      {
+        answerText:
+          "Add a lead to a Salesforce campaign synced to a Marketo program",
+        isCorrect: false,
+      },
+      {
+        answerText:
+          "Select the acquisition program when importing into the Lead Database",
+        isCorrect: false,
+      },
+    ],
+  },
+];
