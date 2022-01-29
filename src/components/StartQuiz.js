63981abade9e9a104101050a626dfa67de6987ee
nelsonh88 <nelson.huezo88@gmail.com>
2022-01-29 13:32:10 -0500

    Adds styling to the StartQuiz component     - Added a hr under the h1     - Added styling for the H1 and p tag in StartQuiz component

--- src/components/StartQuiz.js
+++ src/components/StartQuiz.js
@@ -2,14 +2,15 @@ import React from "react";
 
 const StartQuiz = (props) => {
   return (
-    <React.Fragment>
+    <div className="start-quiz">
       <h1>Marketo Certification Practice Quiz</h1>
+      <div className="divider"></div>
       <p>
         The following practice quiz is a compilation of questions that will
         serve as an supplement to your studying.
       </p>
       <button onClick={props.start}>Start Quiz</button>
-    </React.Fragment>
+    </div>
   );
 };
 
